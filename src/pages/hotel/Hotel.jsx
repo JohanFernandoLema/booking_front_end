fae2792b2bdd3a56153ef12b573193ae4ecabdbc
JohanFernandoLema <johan.13fernando@gmail.com>
2023-09-10 10:27:50 -0400

    typo fixed in order to retrieve hotel name

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -82,7 +82,7 @@ const Hotel = () => {
           )}
           <div className="hotelWrapper">
             <button className="bookNow">Reserve or Book Now!</button>
-            <h1 className="hotelTitle">data.name</h1>
+            <h1 className="hotelTitle">{data.name}</h1>
             <div className="hotelAddress">
               <FontAwesomeIcon icon={faLocationDot} />
               <span>{data.address}</span>
